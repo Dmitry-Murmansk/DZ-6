f85c3cf1b4662fcb4c498c9e90f68b00b912c3eb
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-11 18:40:26 +0300

    seminar6; zadacha2

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.Write($"{x}, Y: {y}");
+ //Напишите программу, которая найдёт точку пересечения 
+ //двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+ //значения b1, k1, b2 и k2 задаются пользователем.
